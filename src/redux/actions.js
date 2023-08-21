c186419a9fcc8a7f7a70339ef615bfd78a2c72e7
Rammbest <igor-kutsenko@ukr.net>
2023-08-21 22:48:00 +0300

    refactoring with redux-toolkit

---
+++ src/redux/actions.js
@@ -0,0 +1,37 @@
+// import { createAction, nanoid } from '@reduxjs/toolkit';
+
+// export const addTask = createAction('tasks/addTask', text => {
+//   return {
+//     payload: {
+//       id: nanoid(),
+//       completed: false,
+//       text,
+//     },
+//   };
+// });
+
+// export const deleteTask = createAction('tasks/deleteTask');
+
+// export const toggleCompleted = createAction('tasks/toggleCompleted');
+
+// export const setStatusFilter = createAction('filters/setStatusFilter');
+
+// export const deleteTask = taskId => {
+//   return {
+//     type: 'tasks/deleteTask',
+//     payload: taskId,
+//   };
+// };
+
+// export const toggleCompleted = taskId => {
+//   return {
+//     type: 'tasks/toggleCompleted',
+//     payload: taskId,
+//   };
+// };
+// export const setStatusFilter = value => {
+//   return {
+//     type: 'filters/setStatusFilter',
+//     payload: value,
+//   };
+// };
---
+++ src/redux/filtersSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { statusFilters } from './contans';
+
+const filtersInitialState = {
+  status: statusFilters.all,
+};
+
+const filtersSlice = createSlice({
+  name: 'filters',
+  initialState: filtersInitialState,
+  reducers: {
+    setStatusFilter(state, action) {
+      return {
+        ...state,
+        status: action.payload,
+      };
+    },
+  },
+});
+
+export const { setStatusFilter } = filtersSlice.actions;
+export const filtersReducer = filtersSlice.reducer;
